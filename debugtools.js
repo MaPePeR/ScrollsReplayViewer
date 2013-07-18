2c8e98c945a9125ba5e17990e9020007d0f62b6d
MaPePeR <mapeper@example.com>
2013-07-18 20:56:48 +0200

    Manual Placement

--- debugtools.js
+++ debugtools.js
@@ -2,20 +2,32 @@
 /*global replayreader, $:false*/
 function fillBoard() {
     "use strict";
+    // width/height = 16/15
     var fw = $('#fieldwhite'), rowW, scrollW;
     var fb = $('#fieldblack'), rowB, scrollB;
     var y, x;
+    var width = fw.width(), height = fw.height();
+    var idolW, idolB;
+    width  = Math.min(width, 16 * height / 15);
+    height = Math.min(height, 15 * width / 16);
+    var isBackRow;
     for (y = 0; y < 5; y += 1) {
-        rowW = $('<div class="fieldrow"></div>');
-        rowB = $('<div class="fieldrow"></div>');
+        isBackRow = y % 2 === 1;
+        idolW = $('<div class="idol" id="whiteidol' + y + '">20</div>');
+        idolW.height(height / 5).width(width / 8).css('top', y * height / 5).css('left', 0);
+        idolB = $('<div class="idol" id="blackidol' + y + '">20</div>');
+        idolB.height(height / 5).width(width / 8).css('top', y * height / 5).css('right', 0);
         for (x = 0; x < 3; x += 1) {
             scrollW = $('<img class="fieldscroll" src="http://www.scrollsguide.com/app/low_res/810.png">');
+            scrollW.width(width / 4).css('top', y * height / 5).css('left', (isBackRow ? width / 8 : width / 4) + x * width / 4);
+            fw.append(scrollW);
+
             scrollB = $('<img class="fieldscroll" src="http://www.scrollsguide.com/app/low_res/719.png">');
-            rowW.append(scrollW);
-            rowB.append(scrollB);
+            scrollB.width(width / 4).css('top', y * height / 5).css('right', (isBackRow ? width / 8 : width / 4) + x * width / 4);
+            fb.append(scrollB);
         }
-        fw.append(rowW);
-        fb.append(rowB);
+        fw.append(idolW);
+        fb.append(idolB);
     }
 }
 $(fillBoard);
