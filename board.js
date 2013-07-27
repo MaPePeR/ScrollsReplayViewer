218f03f3bee2863a42a9f6433d3679ff48babb27
MaPePeR <mapeper@example.com>
2013-07-27 21:00:03 +0200

    board.init function

--- board.js
+++ board.js
@@ -36,6 +36,93 @@
         };
     }
 
+    function generateIdols() {
+        var idolW, idolB;
+        var fw = board.white.fieldElem;
+        var fb = board.black.fieldElem;
+        var width = board.lastwidth, height = board.lastheight, y;
+        width  = Math.min(width, 16 * height / 15);
+        height = Math.min(height, 15 * width / 16);
+
+        for (y = 0; y < 5; y += 1) {
+            idolW = $('<div class="idol" id="whiteidol' + y + '"></div>');
+            idolW.height(height / 5).width(width / 8).css('top', y * height / 5);
+            board.white.idols[y] = idolW;
+            fw.append(idolW);
+
+            idolB = $('<div class="idol" id="blackidol' + y + '"></div>');
+            idolB.height(height / 5).width(width / 8).css('top', y * height / 5);
+            board.black.idols[y] = idolB;
+            fb.append(idolB);
+        }
+    }
+
+    var backgroundCtx;
+    function drawBackground() {
+        //Adjust canvas size
+        var width  = backgroundCtx.canvas.width  = $('#gamefield').innerWidth();
+        var height = backgroundCtx.canvas.height = $('#gamefield').innerHeight();
+        var fwidth = width / 2.0;
+        backgroundCtx.fillStyle = 'green';
+        backgroundCtx.fillRect(0, 0, width, height);
+        backgroundCtx.strokeStyle = 'black';
+        var x, y;
+        for (y = 0; y < 5; y += 1) {
+            for (x = 0; x < 3; x += 1) {
+                backgroundCtx.strokeRect((y % 2 === 1 ? fwidth / 8 : fwidth / 4) + x * fwidth / 4, y * height / 5, fwidth / 4, fwidth * 3 / 4 / 4);
+            }
+        }
+    }
+
+    function calcSize() {
+        var y, x;
+        var width = (board.lastwidth = board.white.fieldElem.width());
+        var height = (board.lastheight = board.white.fieldElem.height());
+        for (y = 0; y < 5; y += 1) {
+            for (x = 0; x < 3; x += 1) {
+                if (board.white.field[y][x] !== undefined) {
+                    board.white.field[y][x].width(width / 4).height(width * 3 / 4 / 4).css('top', y * height / 5).css(replayreader.getPerspective() === 'white' ? 'left' : 'right', (y % 2 === 1 ? width / 8 : width / 4) + x * width / 4);
+                }
+                if (board.black.field[y][x] !== undefined) {
+                    board.black.field[y][x].width(width / 4).height(width * 3 / 4 / 4).css('top', y * height / 5).css(replayreader.getPerspective() === 'black' ? 'left' : 'right', (y % 2 === 1 ? width / 8 : width / 4) + x * width / 4);
+                }
+            }
+            board.black.idols[y].height(height / 5).width(width / 8).css('top', y * height / 5);
+            board.white.idols[y].height(height / 5).width(width / 8).css('top', y * height / 5);
+        }
+        drawBackground();
+    }
+
+    $(window).resize(calcSize);
+
+
+
+    exports.init = function () {
+        backgroundCtx = $("#fieldbackground")[0].getContext('2d');
+        if (board !== undefined) {
+            var x, y;
+            for (y = 0; y < 5; y += 1) {
+                for (x = 0; x < 3; x += 1) {
+                    if (board.white.field[y][x] !== undefined) {
+                        board.white.field[y][x].remove();
+                    }
+                    if (board.black.field[y][x] !== undefined) {
+                        board.black.field[y][x].remove();
+                    }
+                }
+                board.black.idols[y].remove();
+                board.white.idols[y].remove();
+            }
+            board = undefined;
+        }
+        board = emptyBoard();
+        generateIdols();
+        calcSize();
+    };
+    /* 
+        Effect functions
+    */
+
     exports.summonUnit = function (target, cardTypeId, callback) {
         var elem = $('<div class="fieldscroll"><input type="text" class="attack" disabled/><input type="text" class="countdown" disabled/><input type="text" class="health" disabled/></div>').css('background-image', 'url(' + images.getMainImageURLForScroll(cardTypeId) + ')');
         var width = board.lastwidth, height = board.lastheight;
--- replayviewer.js
+++ replayviewer.js
@@ -1,5 +1,5 @@
 /*jslint browser: true, vars: true, eqeq: true*/
-/*global alert: false, $: false, replayreader: false*/
+/*global alert: false, $: false, replayreader, board: false*/
 
 if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
     alert('The File APIs are not fully supported in this browser.');
@@ -7,68 +7,6 @@ if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
 
 $(function () {
     "use strict";
-
-    var backgroundCtx = $("#fieldbackground")[0].getContext('2d');
-    function drawBackground() {
-        //Adjust canvas size
-        var width  = backgroundCtx.canvas.width  = $('#gamefield').innerWidth();
-        var height = backgroundCtx.canvas.height = $('#gamefield').innerHeight();
-        var fwidth = width / 2.0;
-        backgroundCtx.fillStyle = 'green';
-        backgroundCtx.fillRect(0, 0, width, height);
-        backgroundCtx.strokeStyle = 'black';
-        var x, y;
-        for (y = 0; y < 5; y += 1) {
-            for (x = 0; x < 3; x += 1) {
-                backgroundCtx.strokeRect((y % 2 === 1 ? fwidth / 8 : fwidth / 4) + x * fwidth / 4, y * height / 5, fwidth / 4, fwidth * 3 / 4 / 4);
-            }
-        }
-    }
-
-    function calcSize() {
-        console.log('resize!');
-        var y, x;
-        var width = (board.lastwidth = $('#fieldwhite').width());
-        var height = (board.lastheight = $('#fieldwhite').height());
-        for (y = 0; y < 5; y += 1) {
-            for (x = 0; x < 3; x += 1) {
-                if (board.whitefield[y][x] !== undefined) {
-                    board.whitefield[y][x].width(width / 4).height(width * 3 / 4 / 4).css('top', y * height / 5).css(replayreader.getPerspective() === 'white' ? 'left' : 'right', (y % 2 === 1 ? width / 8 : width / 4) + x * width / 4);
-                }
-                if (board.blackfield[y][x] !== undefined) {
-                    board.blackfield[y][x].width(width / 4).height(width * 3 / 4 / 4).css('top', y * height / 5).css(replayreader.getPerspective() === 'black' ? 'left' : 'right', (y % 2 === 1 ? width / 8 : width / 4) + x * width / 4);
-                }
-            }
-            board.blackIdols[y].height(height / 5).width(width / 8).css('top', y * height / 5);
-            board.whiteIdols[y].height(height / 5).width(width / 8).css('top', y * height / 5);
-        }
-        handcards.moveCards();
-        drawBackground();
-    };
-    $(window).resize(calcSize);
-
-    board = emptyBoard();
-    function generateIdols() {
-        var idolW, idolB;
-        var fw = board.whitefieldElem;
-        var fb = board.blackfieldElem;
-        var width = board.lastwidth, height = board.lastheight, y;
-        width  = Math.min(width, 16 * height / 15);
-        height = Math.min(height, 15 * width / 16);
-
-        for (y = 0; y < 5; y += 1) {
-            idolW = $('<div class="idol" id="whiteidol' + y + '"></div>');
-            idolW.height(height / 5).width(width / 8).css('top', y * height / 5);
-            board.whiteIdols[y] = idolW;
-            fw.append(idolW);
-
-            idolB = $('<div class="idol" id="blackidol' + y + '"></div>');
-            idolB.height(height / 5).width(width / 8).css('top', y * height / 5);
-            board.blackIdols[y] = idolB;
-            fb.append(idolB);
-        }
-    }
-    generateIdols();
 
     var filenamePattern = /\.s[gp]r$/; //For ScrollsGuide(.sgr) and ScrollsPost(.spr)-Replays
     function validateReplayFiles() {
@@ -97,8 +35,8 @@ $(function () {
         var fileR = document.getElementById("replayfileR").files[0];
         replayreader.init(fileL, fileR, function () {
             $("#replaychooser").hide();
+            board.init();
             $("#game").show();
-            calcSize();
             $("#playernamewhite").text(replayreader.getWhiteName());
             $("#playernameblack").text(replayreader.getBlackName());
             //Default: white player on the left. when perspective is 'black': swap postions of GUI elements
